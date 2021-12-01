91d2d82be3bf02c4845cdb19ee71937a616b4c26
Julian Alzate <andres_alzate82142@elpoli.edu.co>
2021-12-01 18:53:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import soap from "jquery.soap";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    soap({
+      url: "https://frozen-river-98217.herokuapp.com/index.php?wsdl",
+      method: "saludar",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+
+      data: {
+        name: "Remy Blom",
+      },
+
+      success: function (soapResponse) {
+        // do stuff with soapResponse
+        // if you want to have the response as JSON use soapResponse.toJSON();
+        // or soapResponse.toString() to get XML string
+        // or soapResponse.toXML() to get XML DOM
+        console.log("RES: ", soapResponse.toXML());
+        setResponse(soapResponse.toXML());
+      },
+      error: function (SOAPResponse) {
+        // show error
+        console.log("ERROR: ", SOAPResponse);
+      },
+    });
+  }, []);
+
+  /*  if (response) {
+    return response;
+  }
+ */
   return (
     <div className="App">
       <header className="App-header">
