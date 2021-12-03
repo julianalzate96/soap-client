761b3d7a7dcc46c6bc47d9fe3befa410c0eff16b
julianalzate96 <julian.alzate@parrolabs.com>
2021-12-02 23:29:12 -0500

    App updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import soap from "jquery.soap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import XMLViewer from "react-xml-viewer";
 
-import logo from "./logo.svg";
-import "./App.css";
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/test" element={<Test />} />
+        <Route path="/service" element={<Service />} />
+      </Routes>
+    </Router>
+  );
+}
 
-function App() {
+function Service() {
   const [response, setResponse] = useState(null);
 
   useEffect(() => {
     soap({
-      url: "https://frozen-river-98217.herokuapp.com/index.php?wsdl",
+      url: "http://192.168.1.6:80/test/index.php?wsdl",
       method: "saludar",
       headers: {
         "Access-Control-Allow-Origin": "*",
@@ -20,42 +30,24 @@ function App() {
       },
 
       success: function (soapResponse) {
-        // do stuff with soapResponse
-        // if you want to have the response as JSON use soapResponse.toJSON();
-        // or soapResponse.toString() to get XML string
-        // or soapResponse.toXML() to get XML DOM
         console.log("RES: ", soapResponse.toXML());
         setResponse(soapResponse.toXML());
       },
       error: function (SOAPResponse) {
-        // show error
         console.log("ERROR: ", SOAPResponse);
       },
     });
   }, []);
 
-  /*  if (response) {
-    return response;
-  }
- */
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
-export default App;
+function Test() {
+  const xml = `<note>
+  <to>Tove</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+  </note>`;
+  return <XMLViewer xml={xml} collapsible />;
+}
