50fb5df6cba2461d9d3fa5f38c8dd650793011cc
Julian Alzate <julian.alzate@parrolabs.com>
2022-01-08 03:13:35 -0500

    modal refactored

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import _soap from "jquery.soap";
 import XMLViewer from "react-xml-viewer";
 // import parseXML from "xml-parse-from-string";
@@ -6,34 +6,57 @@ import XMLViewer from "react-xml-viewer";
 import ClickHandler from "../../hooks/clickHandler";
 import Layout from "./layout";
 import Form from "../Form";
+import serviceContext from "../../context/serviceContext";
 
 const customTheme = {
   overflowBreak: true,
+  tagColor: "#2815B2",
 };
 
-export default function Modal({ service, setShowModal }) {
-  const onSubmit = () => {
-    // _soap({
-    //   url: service.wsdl,
-    //   method: service.name,
-    //   headers: { "Content-Type": "application/xml" },
-    //   data: {},
-    //   success: function (soapResponse) {
-    //     // setResponse(soapResponse.toXML());
-    //     // setXML(soapResponse.toString());
-    //   },
-    //   error: function (SOAPResponse) {
-    //     console.log("ERROR: ", SOAPResponse);
-    //   },
-    // });
+export default function Modal({ selectedService, setShowModal }) {
+  const { service } = useContext(serviceContext);
+  const [response, setResponse] = useState({ response: null, xml: "" });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    let data = {};
+
+    if (selectedService.input) {
+      for (let i = 0; i < e.target.elements.length; i++) {
+        if (e.target.elements[i].nodeName !== "BUTTON") {
+          data = {
+            ...data,
+            [e.target.elements[i].name]: e.target.elements[i].value,
+          };
+        }
+      }
+    }
+
+    _soap({
+      url: service.wsdl,
+      method: selectedService.name,
+      headers: { "Content-Type": "application/xml" },
+      data,
+      success: function (soapResponse) {
+        setResponse({
+          response: soapResponse.toXML(),
+          xml: soapResponse.toString(),
+        });
+      },
+      error: function (SOAPResponse) {
+        console.log("ERROR: ", SOAPResponse);
+      },
+    });
   };
 
   const renderInputs = () => {
-    if (service.input) {
-      return Object.keys(service.input).map((input) => {
+    if (selectedService.input) {
+      return Object.keys(selectedService.input).map((input, i) => {
         return (
           <Form.Input
-            type={service.input[input].replace("xsd:", "")}
+            key={i}
+            type={selectedService.input[input].replace("xsd:", "")}
             label={input}
           />
         );
@@ -46,23 +69,31 @@ export default function Modal({ service, setShowModal }) {
     <Layout>
       <ClickHandler action={setShowModal}>
         <div className="body">
-          <h3>{service.name}</h3>
-          <p>{service.description}</p>
+          <h3>{selectedService.name}</h3>
+          <p>{selectedService.description}</p>
           <Form onSubmit={onSubmit}>{renderInputs()}</Form>
-          {service.xml_entradas && (
+          {response.response && (
             <XMLViewer
-              xml={service.xml_entradas}
+              xml={response.xml}
               theme={customTheme}
               collapsible
-              indentSize
+              indentSize={1}
             />
           )}
-          {service.xml_respuestas && (
+          {selectedService.xml_entradas && (
             <XMLViewer
-              xml={service.xml_respuestas}
+              xml={selectedService.xml_entradas}
               theme={customTheme}
               collapsible
-              indentSize
+              indentSize={1}
+            />
+          )}
+          {selectedService.xml_respuestas && (
+            <XMLViewer
+              xml={selectedService.xml_respuestas}
+              theme={customTheme}
+              collapsible
+              indentSize={1}
             />
           )}
         </div>
