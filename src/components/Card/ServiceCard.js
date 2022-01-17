18e74b534f10c031e30e97c8fbd29daaf8c1e59c
Julian Alzate <julian.alzate@parrolabs.com>
2022-01-17 01:44:32 -0500

    service card updated

--- src/components/Card/ServiceCard.js
+++ src/components/Card/ServiceCard.js
@@ -5,11 +5,14 @@ import "../../styles/_service_card.scss";
 export default function ServiceCard({ service, onClick }) {
   return (
     <div className="service-card" onClick={onClick}>
-      <h3>{service.name}</h3>
+      <h3>{service.nombre}</h3>
       <p>
-        {service.description ||
+        {service.descripcion ||
           "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}
       </p>
+      <a className="wsdl" href={service.wsdl} target="_blank" rel="noreferrer">
+        {service.wsdl}
+      </a>
     </div>
   );
 }
