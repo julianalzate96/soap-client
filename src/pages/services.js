5d4d579b32c5c8919283eff209316fbcb9fa44c4
Julian Alzate <julian.alzate@parrolabs.com>
2022-01-17 01:45:04 -0500

    service page updated

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from "react";
-import soap from "soap-everywhere";
 import Lottie from "lottie-react";
 
 import { fetchServicesInfo } from "../api";
@@ -20,41 +19,16 @@ export default function Services() {
   const [selectedService, setSelectedService] = useState({});
 
   const fetchCurrentCategoryServices = (service) => {
-    if (service.wsdl.length > 0) {
-      setBusy(true);
-      soap.createClient(service.wsdl, (err, client) => {
-        let response = null;
-        let _services = [];
-
-        if (err) {
-          console.log("ERROR: ", err);
-          setError(true);
-        }
-
-        response = client.describe();
-
-        if (response) {
-          _services = Object.keys(
-            response[service.name][`${service.name}Port`]
-          ).map((key) => {
-            return {
-              name: key,
-              ...response[service.name][`${service.name}Port`][key],
-            };
-          });
-        }
-
-        fetchServicesInfo("services", service.id).then((res) => {
-          let ser = _services.map((se) => {
-            let found = res.find((s) => s.nombre === se.name);
-            return { ...se, ...found };
-          });
-
-          setServices(ser);
-        });
+    setBusy(true);
+    fetchServicesInfo("services", service.id)
+      .then((res) => {
+        setServices(res);
+        setBusy(false);
+      })
+      .catch((err) => {
+        setError(err);
         setBusy(false);
       });
-    }
   };
 
   const handleSelectedService = (service) => {
@@ -89,7 +63,6 @@ export default function Services() {
   return (
     <Layout>
       <h1>{service.name}</h1>
-      <a href={service.wsdl}>{service.wsdl}</a>
       <p>{service.description}</p>
       <div className="container">
         {error && (
