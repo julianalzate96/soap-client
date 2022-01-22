1f931df4ec3e1d8b4e2d445b4f555065bbaf1434
Julian Alzate <julian.alzate@parrolabs.com>
2022-01-21 20:05:25 -0500

    header links aded

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import APIT from "../../images/APIT-logo.jpeg";
 
 import "../../styles/_header.scss";
@@ -8,13 +8,21 @@ export default function Header() {
   return (
     <header className="header">
       <div className="logo-container">
-        <img
-          src="https://www.politecnicojic.edu.co/images/logo/logo.png"
-          alt="Logo PCJIC"
-        />
+        <Link to="/">
+          <img
+            src="https://www.politecnicojic.edu.co/images/logo/logo.png"
+            alt="Logo PCJIC"
+          />
+        </Link>
       </div>
       <div className="logo-apit-container">
-        <img src={APIT} alt="Logo APIT" />
+        <a
+          href="https://web.facebook.com/apitpolijic/?_rdc=1&_rdr"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={APIT} alt="Logo APIT" />
+        </a>
       </div>
     </header>
   );
