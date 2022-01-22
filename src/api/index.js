b3305d7919a756c7b9e97911ae8eb7fac8de1c53
Julian Alzate <julian.alzate@parrolabs.com>
2022-01-22 01:00:58 -0500

    new api funcion added for get token

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,25 @@
 import axios from "axios";
 
+export const HEROKU_URL = "https://frozen-river-98217.herokuapp.com";
+
 export const fetchServicesInfo = (type, category) => {
   const params = `type=${type}${category ? `&category=${category}` : ""}`;
   return axios
-    .get(`https://frozen-river-98217.herokuapp.com/uddi.php/?${params}`)
+    .get(`${HEROKU_URL}/uddi.php/?${params}`)
     .then((response) => response.data)
-    .then((data) => data)
+    .catch(function (error) {
+      console.log(error);
+      return error;
+    });
+};
+
+export const getServicesToken = (data) => {
+  return axios({
+    url: `${HEROKU_URL}/auth.php`,
+    method: "POST",
+    data,
+  })
+    .then((response) => response.data)
     .catch(function (error) {
       // handle error
       console.log(error);
