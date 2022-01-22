e956a541b51cb1588f7e6d2e2628d2c592beafdd
Julian Alzate <julian.alzate@parrolabs.com>
2022-01-21 21:54:23 -0500

    private service validations added

--- src/components/Card/ServiceCard.js
+++ src/components/Card/ServiceCard.js
@@ -3,16 +3,31 @@ import React from "react";
 import "../../styles/_service_card.scss";
 
 export default function ServiceCard({ service, onClick }) {
+  let isPrivado = service.privado === "1";
   return (
-    <div className="service-card" onClick={onClick}>
+    <div
+      className={`service-card ${isPrivado ? `priv-service` : ``}`}
+      onClick={!isPrivado ? onClick : () => {}}
+    >
       <h3>{service.nombre}</h3>
       <p>
         {service.descripcion ||
           "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."}
       </p>
-      <a className="wsdl" href={service.wsdl} target="_blank" rel="noreferrer">
-        {service.wsdl}
+      <a
+        className="wsdl"
+        href={!isPrivado ? `#privado` : service.wsdl}
+        target="_blank"
+        rel="noreferrer"
+      >
+        {!isPrivado ? service.wsdl : `WSDL NO DISPONIBLE`}
       </a>
+
+      {isPrivado && (
+        <span>
+          <strong>PRIVADO</strong>
+        </span>
+      )}
     </div>
   );
 }
