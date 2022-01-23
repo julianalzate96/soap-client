04c797771d1b65655242601f1ae783b008af439f
Julian Alzate <julian.alzate@parrolabs.com>
2022-01-23 03:32:35 -0500

    api call updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,45 @@
 import axios from "axios";
 
+import {
+  fetchCategoriesFailure,
+  fetchCategoriesRequest,
+  fetchCategoriesSuccess,
+} from "../redux/actions/category.actions";
+
 export const HEROKU_URL = "https://frozen-river-98217.herokuapp.com";
 
-export const fetchServicesInfo = (type, category) => {
-  const params = `type=${type}${category ? `&category=${category}` : ""}`;
-  return axios
-    .get(`${HEROKU_URL}/uddi.php/?${params}`)
+export const fetchCategories = (dispatch) => {
+  dispatch(fetchCategoriesRequest());
+  axios
+    .get(`${HEROKU_URL}/uddi.php/?type=categories`)
+    .then((response) => {
+      // console.log(response.data);
+      dispatch(fetchCategoriesSuccess(response.data));
+    })
+    .catch((error) => {
+      // console.log(error);
+      dispatch(fetchCategoriesFailure());
+    });
+};
+
+export const fetchServicesInfo = (id_category) => {
+  axios
+    .get(`${HEROKU_URL}/uddi.php/?type=service&category=${id_category}`)
     .then((response) => response.data)
-    .catch(function (error) {
+    .catch((error) => {
       console.log(error);
-      return error;
     });
 };
 
 export const getServicesToken = (data) => {
-  return axios({
+  axios({
     url: `${HEROKU_URL}/auth.php`,
     method: "POST",
     data,
   })
     .then((response) => response.data)
-    .catch(function (error) {
+    .catch((error) => {
       // handle error
       console.log(error);
-      return error;
     });
 };
